a761a59cce592d1cc95ac70be83abbe31df2fc9f
Vitório Aflalo <vitorioaflalo86@live.com>
2023-01-18 17:46:38 -0300

    arquivo app.py criado e inicializado

---
+++ app.py
@@ -0,0 +1,15 @@
+from src.extraction import load_data
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+def main():
+    df_raw = load_data()
+
+    st.dataframe(df_raw)
+
+if __name__ == '__main__':
+
+    main()
+
+
